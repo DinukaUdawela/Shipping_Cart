cfe889ff00920ad01f469f86f65948ee834258a1
DinukaUdawela <126551484+DinukaUdawela@users.noreply.github.com>
2024-08-30 00:41:14 +0530

    add products

--- backend/data.js
+++ backend/data.js
@@ -7,9 +7,9 @@ const products = [
     { id: 5, name: 'Apple', category: 'Food', image: '/images/apple.jpg' },
     { id: 6, name: 'Denim', category: 'Clothing', image: '/images/denim.jpg' },
     { id: 7, name: 'Navel', category: 'Books', image: '/images/navel.jpg' },
-    { id: 8, name: 'Camera', category: 'Electronics', image: '/images/camera.jpg' },
+    { id: 8, name: 'Camera', category: 'Electronics', image: '/images/camera.png' },
     { id: 9, name: 'Orange', category: 'Food', image: '/images/orange.jpg' },
-    { id: 10, name: 'Smartwatch', category: 'Electronics', image: '/images/smartwatch.jpg' },
+    { id: 10, name: 'Smartwatch', category: 'Electronics', image: '/images/watch.png' },
     { id: 11, name: 'Running Shoes', category: 'Clothing', image: '/images/shoes.jpg' },
     { id: 12, name: 'Cookbook', category: 'Books', image: '/images/cookbook.jpg' }
 ];
